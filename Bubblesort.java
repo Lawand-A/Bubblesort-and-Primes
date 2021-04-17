542d67fd52812fa0c22b180b32b77d286e179c92
Lawand-A <76704719+Lawand-A@users.noreply.github.com>
2021-04-17 16:35:22 +0200

    Update Bubblesort.java

--- Bubblesort.java
+++ Bubblesort.java
@@ -26,7 +26,7 @@ public class Bubblesort {
 //The Bubble Sort algorithm
 	public static void bubbleSort(int[] array) {
 		int tmp;
-		for(int i = 0; i < array.length; i++) {
+		for(int i = 0; i < array.length-1; i++) {
 			for (int j = 0; j < array.length-1; j++) {
 				if(array[j] < array[j+1]) {
 					tmp = array[j+1];
